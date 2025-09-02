6a88585e1cbe45a2ea297ebcf2cfad033e52c9c9
RenugadeviG <renugopaldada@gmail.com>
2025-09-02 17:20:54 +0530

    Update brc (1).py

--- brc (1).py
+++ brc (1).py
@@ -3,7 +3,7 @@
 
 # ✅ Use TensorFlow Lite runtime
 import numpy as np
-from tensorflow.lite.python.interpreter import Interpreter
+from tflite_runtime.interpreter import Interpreter
 from PIL import Image
 
 # Load TFLite model
